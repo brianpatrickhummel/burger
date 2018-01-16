d6b4fa6bc70e6027593150e9643cb2b869f08d74
brianpatrickhummel <frankgucci@mac.com>
2018-01-15 21:17:26 -0800

    update heroku app name

--- config/connection.js
+++ config/connection.js
@@ -4,20 +4,16 @@ var connection;
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    port: 8889,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-  });
+  connection = mysql.createConnection({host: "localhost", port: 8889, user: "root", password: "root", database: "burgers_db"});
 }
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-  //CALLFIRSTFUNCTION
-});
+connection
+  .connect(function (err) {
+    if (err) 
+      throw err;
+    console.log("connected as id " + connection.threadId);
+    //CALLFIRSTFUNCTION
+  });
 
 // Export connection for our ORM to use.
 module.exports = connection;
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,26 +2,28 @@ var burger = require("../models/burger.js");
 var express = require("express");
 var router = express.Router();
 
-router.get("/", function(req, res) {
-  burger.all(function(data) {
-    var burgersObj = {
-      burgers: data
-    };
-    res.render("index", burgersObj);
-  });
+router.get("/", function (req, res) {
+  burger
+    .all(function (data) {
+      var burgersObj = {
+        burgers: data
+      };
+      res.render("index", burgersObj);
+    });
 });
 
-router.post("/", function(req, res) {
-  burger.create("burger_name", req.body.burger_name, function() {
-    res.redirect("/");
-  });
+router.post("/", function (req, res) {
+  burger
+    .create("burger_name", req.body.burger_name, function () {
+      res.redirect("/");
+    });
 });
 
-router.put("/:id", function(req, res) {
+router.put("/:id", function (req, res) {
   var devoured = req.body.devour;
   console.log("devoured = " + devoured);
   var condition = "id = " + req.params.id;
-  burger.update("devoured", devoured, condition, function() {
+  burger.update("devoured", devoured, condition, function () {
     res.redirect("/");
   });
 });
